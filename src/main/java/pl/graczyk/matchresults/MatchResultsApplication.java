fce8ff9e00f728542f5d13c0f5de73a5c360c51f
Natalia Graczyk <natka.graczyk@gmail.com>
2023-01-18 10:53:57 +0100

    Initial commit

---
+++ src/main/java/pl/graczyk/matchresults/MatchResultsApplication.java
@@ -0,0 +1,14 @@
+package pl.graczyk.matchresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchResultsApplication {
+
+    public static void main(String[] args) {
+      //  SpringApplication.run(MatchResultsApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/pl/graczyk/matchresults/MatchResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.graczyk.matchresults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchResultsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
