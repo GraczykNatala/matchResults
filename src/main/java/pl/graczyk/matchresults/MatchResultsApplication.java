7427e28d6dae442431bd2a7d076122cb2da0fb61
Natalia Graczyk <natka.graczyk@gmail.com>
2023-01-19 12:00:32 +0100

    modify method to handle amount of matches given as a parameter

--- src/main/java/pl/graczyk/matchresults/MatchResultsApplication.java
+++ src/main/java/pl/graczyk/matchresults/MatchResultsApplication.java
@@ -1,46 +1,19 @@
 package pl.graczyk.matchresults;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import static java.util.Arrays.sort;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 
 public class MatchResultsApplication {
 
     public static void main(String[] args) throws IOException {
 
-        DataService ds = new DataService();
-        List<Event> events = ds.eventList();
-        events.stream().forEach(e -> e.setHighest_probable_result(Event.HighestProbableResult(e.getProbability_home_team_winner(),
-                e.getProbability_draw(),e.getProbability_away_team_winner())));
-        System.out.println("SORTOWANIE");
 
-        int amountOfResults = 10;
 
-      Collections.sort(events);
-        showList(amountOfResults, events);
-
+        Scanner scanner = new Scanner(System.in);
+        int amountOfResults = scanner.nextInt();
+        MatchResultsService matchResutsService = new MatchResultsService();
+        matchResutsService.ListOfMatchResults(amountOfResults);
 
         }
-
-    private static void showList(int amountOfResults, List<Event> events) {
-        for (int i = 0; i< 10; i++){
-            System.out.println(events.get(i));
-        }
-    }
-
-
 
 }
---
+++ src/main/java/pl/graczyk/matchresults/MatchResultsService.java
@@ -0,0 +1,33 @@
+package pl.graczyk.matchresults;
+
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.List;
+
+public class MatchResultsService {
+
+    private final DataService dataService = new DataService();
+
+    public MatchResultsService() {
+    }
+    public void ListOfMatchResults(int amountOfResults) throws FileNotFoundException {
+        List<Event> events = dataService.eventList();
+        events.stream().forEach(e -> e.setHighest_probable_result(Event.HighestProbableResult(e.getProbability_home_team_winner(),
+                e.getProbability_draw(),e.getProbability_away_team_winner())));
+        Collections.sort(events);
+
+        try {
+            showList(amountOfResults, events);
+        } catch (IndexOutOfBoundsException e) {
+            amountOfResults = events.size();
+        }
+    }
+
+
+
+    private void showList(int amountOfResults, List<Event> events) {
+        for (int i = 0; i< amountOfResults; i++){
+            System.out.println(events.get(i));
+        }
+    }
+}
