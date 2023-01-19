c7c24b5191fd6c9434638064364323efa025c269
Natalia Graczyk <natka.graczyk@gmail.com>
2023-01-19 11:16:26 +0100

    create class DataService

--- src/main/java/pl/graczyk/matchresults/DataService.java
+++ src/main/java/pl/graczyk/matchresults/DataService.java
@@ -1,11 +1,9 @@
 package pl.graczyk.matchresults;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
